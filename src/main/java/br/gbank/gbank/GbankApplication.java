509846f71b33d862bbd85a84462f0e74214b8db5
Helis Freitas <dfhelis@gmail.com>
2021-10-27 08:26:31 -0300

    estrutura imicial

---
+++ src/main/java/br/gbank/gbank/GbankApplication.java
@@ -0,0 +1,13 @@
+package br.gbank.gbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/gbank/gbank/GbankApplicationTests.java
@@ -0,0 +1,13 @@
+package br.gbank.gbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GbankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
